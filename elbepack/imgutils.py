Taumille/elbe

commit c5e73aadb421d75809262e96264f63fc974d87e8

elbepack: imgutils: protect against race between losetup() and udev
After creation of the loop device through losetup, udev reconfigures it.
Partitions are rescanned, removing and then readding their device nodes.
This introduces a race window where ELBE tries to access the non-existent
partition device nodes after creating the loop device.
The involvment of udev is unnecessary anyways as devtmpfs will properly create
the partition device nodes on its won.

Use a file lock to synchronize the accesses between udev and ELBE.
The lock will be dropped automatically when the filedescriptor is closed
at the end of the contextmanager.
It is not defined if ELBE runs before udev or the other way round, but both
cases are fine for our usecase.

Reported-by: John Ogness <jogness@linutronix.de>
Link: https://systemd.io/BLOCK_DEVICE_LOCKING
Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
Reviewed-by: John Ogness <jogness@linutronix.de>

elbepack/imgutils.py
@@ -3,6 +3,7 @@
 # SPDX-FileCopyrightText: 2024 Linutronix GmbH
 
 import contextlib
+import fcntl
 import subprocess
 
 from elbepack.shellhelper import ELBE_LOGGING, do, run
@@ -16,7 +17,10 @@ def losetup(dev, extra_args=[]):
     ).stdout.decode('ascii').rstrip('\n')
 
     try:
-        yield loopdev
+        with open(loopdev) as f:
+            # protect against races with udev
+            fcntl.flock(f, fcntl.LOCK_EX)
+            yield loopdev
     finally:
         do(['losetup', '--detach', loopdev], check=False)
 

commit 714d12019a55f2e57c5df08c2f6fb4d0dc55a8a4

elbepack: imgutils: properly wait for all udev events in losetup()
grub-install called inside the target expects the /dev/disk/by-uuid/ links to
be set up properly. Otherwise plain device names are used, which in the case of
loop devices does not work at all.
Depending on the timing, the current code could block execution of udev and
therefore prevent the creation of the symlinks.

Reported-by: Thomas Bonnefille <thomas.bonnefille@bootlin.com>
Closes: https://github.com/Linutronix/elbe/issues/431
Fixes: c5e73aadb421 ("elbepack: imgutils: protect against race between losetup() and udev")
Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

elbepack/imgutils.py
@@ -4,9 +4,31 @@
 
 import contextlib
 import fcntl
+import pathlib
 import subprocess
 
 from elbepack.shellhelper import ELBE_LOGGING, do, run
+
+
+def _wait_on_udev_for_device_and_partitions(device):
+    # The callers expect the udev symlinks of the loop device and its
+    # partitions to be present.
+
+    device_name = pathlib.Path(device).name
+    with open(device) as f:
+        # The partition entries in /sys/class/blocks are created by the kernel
+        # and guaranteed to exist after "losetup" returns.
+        # However udev processing triggers a rescan of the partitions, removing
+        # the entries for a short time. Prevent udev from doing so while we iterate.
+        fcntl.flock(f, fcntl.LOCK_EX)
+        partitions = [
+            '/dev/' + entry.name
+            for entry in pathlib.Path('/sys/class/block', device_name).iterdir()
+            if entry.name.startswith(device_name)
+        ]
+
+    # All partitions need to be mentioned explicitly.
+    subprocess.check_call(['udevadm', 'wait', device, *partitions])
 
 
 @contextlib.contextmanager
@@ -17,10 +39,8 @@ def losetup(dev, extra_args=[]):
     ).stdout.decode('ascii').rstrip('\n')
 
     try:
-        with open(loopdev) as f:
-            # protect against races with udev
-            fcntl.flock(f, fcntl.LOCK_EX)
-            yield loopdev
+        _wait_on_udev_for_device_and_partitions(loopdev)
+        yield loopdev
     finally:
         do(['losetup', '--detach', loopdev], check=False)
 